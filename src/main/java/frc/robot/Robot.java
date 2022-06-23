c72c6299a73290949a5eeadcdbcef1f6e38a92f2
MPhillips21 <86633797+MPhillips21@users.noreply.github.com>
2022-06-23 23:00:32 +0000

    started

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -17,6 +17,15 @@ public class Robot extends ProceduralRobot {
   @Override
   public void autonomousProcedure() throws InterruptedException {
     // Write your code here:
-
+    Romi.driveTime(0.3,2);
+    Romi.turnTime(1,1.2);
+    Romi.driveTime(0.3,2);
+    System.out.println("50% chance");
+    Romi.turnTime(-1,1.8);
+    Romi.sleepSeconds(0.2);
+    Romi.driveTime(0.3,2);
+    Romi.driveTime(-0.3,2);
+    Romi.turnTime(1,0.9);
+    Romi.driveTime(0.8,1.618);
   }
 }
